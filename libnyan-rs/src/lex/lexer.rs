fd33ad4557d23c729a708e15ad6b9f6635411766
Simon <simon@systemli.org>
2020-05-18 13:13:37 +0200

    Add callback function

--- libnyan-rs/src/lex/lexer.rs
+++ libnyan-rs/src/lex/lexer.rs
@@ -7,7 +7,6 @@ use logos::Logos;
 
 #[derive(Logos, Debug, PartialEq)]
 enum Token {
-
     #[token("as")]
     AS,
 
@@ -20,23 +19,17 @@ enum Token {
     #[token(",")]
     COMMA,
 
-    #[token(" ", handle_indent())]
-    DEDENT,
-
     #[token(".")]
     DOT,
 
-    #[token("")]
-    ENDFILE,
-
- 	#[regex(r"[\n]")]
+   #[regex(r"[\n]")]
     ENDLINE,
 
     #[token("...")]
     ELLIPSIS,
 
-    #[regex(r"-?[0-9]+\.[0-9]*")]
-    FLOAT,
+    #[regex(r"-?[0-9]+\.[0-9]*", |lex| lex.slice().parse())]
+    Float(f64),
 
     #[token("from")]
     FROM,
@@ -47,14 +40,8 @@ enum Token {
     #[token("import")]
     IMPORT,
 
-    #[token(" ", handle_indent())]
-    INDENT,
-
-    #[token("")]
-    INVALID,
-
-    #[regex(r"(-|0[xX])?[0-9]+")]
-    INT,
+    #[regex("(-|0[xX])?[0-9]+", |lex| lex.slice().parse())]
+    Integer(i64),
 
     #[token("<")]
     LANGLE,
@@ -68,7 +55,7 @@ enum Token {
     #[token("(")]
     LPAREN,
 
-    #[regex(r"[-+*/|%&]|[-+*/|%&]=|=")]
+    #[regex("[-+*/|%&]|[-+*/|%&]=|=")]
     OPERATOR,
 
     #[token("pass")]
@@ -86,12 +73,42 @@ enum Token {
     #[token(")")]
     RPAREN,
 
-    #[regex(r"(\.|[^\])*")]
-    STRING,
-
 	// Logos requires one token variant to handle errors,
     // it can be named anything you wish.
     #[error]
     ERROR,
+
+    // TODO
+    // #[token("")]
+    // INVALID,
+
+    // #[token(" ", handle_indent())]
+    // INDENT,
+
+    // #[token(" ", handle_indent())]
+    // DEDENT,
+
+    // #[token("")]
+    // ENDFILE,
+
+    // #[regex(b"\"(\.|[ˆ\"])*\"")]
+    // STRING,
 }
 
+fn track_brackets() {} 
+
+fn handle_indent() {}
+
+
+#[test]
+fn lexer() {
+    let mut lex = Token::lexer(
+    	concat!(
+            "GameEntity.types = {Building, BuildingMisc}",
+            "Sound.play_delay = 0.0f",
+            // "Sound.sounds = o{"../shared/sounds/creation_sound_337.opus"}",
+            "ResourceCost.amount = {WoodAmount, GoldAmount}",
+            // "Animation.sprite = "./graphics/idle_archery_range.sprite"",
+        ),
+);
+}
