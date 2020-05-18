f72d16378c2e9bb45305ac66e462e72628fef15d
Simon <simon@systemli.org>
2020-05-18 11:02:25 +0200

    Add error-chain

--- libnyan-rs/src/lib.rs
+++ libnyan-rs/src/lib.rs
@@ -1,3 +1,44 @@
+// Copyright 2020-2020 the age-rs authors. See copying.md for legal info.
+
+// Entrypoint for libnyan-rs
+
+// `error_chain!` can recurse deeply
+#![recursion_limit = "1024"]
+
+#[macro_use]
+extern crate error_chain;
+
+// Errors module for error-chain
+mod errors {
+    // Create the Error, ErrorKind, ResultExt, and Result types
+    error_chain! {}
+}
+
+
+use errors::*;
+
+
+fn main() {
+    if let Err(ref e) = run() {
+        println!("error: {}", e);
+
+        for e in e.iter().skip(1) {
+            println!("caused by: {}", e);
+        }
+
+        // The backtrace is not always generated. Try to run this example
+        // with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace() {
+            println!("backtrace: {:?}", backtrace);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
 	#[test]
